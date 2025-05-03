2644570a533c06ebb66a66760c8f3dba385e26a3
saivarshitha891 <varshithasrinivas891@gmail.com>
2025-05-02 23:05:32 -0500

    Update data_cleaning.sql

--- banking-risk-fraud-analytics/sql_queries/data_cleaning.sql
+++ banking-risk-fraud-analytics/sql_queries/data_cleaning.sql
@@ -6,4 +6,14 @@ SELECT
   SUM(CASE WHEN amount IS NULL THEN 1 ELSE 0 END) AS null_amount
 FROM synthetic_transactions;
 
-
+-- 2. Remove duplicates if any
+DELETE FROM synthetic_transactions
+WHERE transaction_id IN (
+  SELECT transaction_id
+  FROM (
+    SELECT transaction_id, COUNT(*) 
+    FROM synthetic_transactions
+    GROUP BY transaction_id
+    HAVING COUNT(*) > 1
+  )
+);
