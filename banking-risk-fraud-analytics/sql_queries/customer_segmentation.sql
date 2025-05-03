cfb313b3ee32057a5af5db031c14916335f2482a
saivarshitha891 <varshithasrinivas891@gmail.com>
2025-05-02 23:14:41 -0500

    Update customer_segmentation.sql

--- banking-risk-fraud-analytics/sql_queries/customer_segmentation.sql
+++ banking-risk-fraud-analytics/sql_queries/customer_segmentation.sql
@@ -8,3 +8,14 @@ SELECT customer_id,
        END AS activity_segment
 FROM synthetic_transactions
 GROUP BY customer_id;
+
+-- Segment customers based on total spending
+SELECT customer_id,
+       SUM(amount) AS total_spent,
+       CASE
+         WHEN SUM(amount) > 50000 THEN 'High Spender'
+         WHEN SUM(amount) BETWEEN 20000 AND 50000 THEN 'Medium Spender'
+         ELSE 'Low Spender'
+       END AS spending_segment
+FROM synthetic_transactions
+GROUP BY customer_id;
