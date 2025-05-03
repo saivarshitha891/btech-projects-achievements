9ed836fd63c38a8236d609b75be444116282636c
saivarshitha891 <varshithasrinivas891@gmail.com>
2025-05-02 23:09:21 -0500

    Update fraud_detection.sql

--- banking-risk-fraud-analytics/sql_queries/fraud_detection.sql
+++ banking-risk-fraud-analytics/sql_queries/fraud_detection.sql
@@ -1,10 +1,17 @@
--- 1. Flag unusually large transactions
-SELECT * FROM synthetic_transactions
-WHERE amount > (
-  SELECT AVG(amount) + 3 * STDDEV(amount) FROM synthetic_transactions
-);
+--Flag unusually large transactions (above 3 standard deviations)
+-- Calculate mean and stddev manually and flag unusually large transactions
+WITH stats AS (
+  SELECT 
+    AVG(amount) AS mean_amount, 
+    SQRT(SUM((amount - (SELECT AVG(amount) FROM synthetic_transactions)) * (amount - (SELECT AVG(amount) FROM synthetic_transactions))) / COUNT(*)) AS stddev_amount
+  FROM synthetic_transactions
+)
+SELECT * 
+FROM synthetic_transactions, stats
+WHERE amount > mean_amount + 3 * stddev_amount;
 
--- 2. Count of frauds by transaction type
+
+-- Count frauds by transaction type
 SELECT transaction_type, COUNT(*) AS fraud_count
 FROM synthetic_transactions
 WHERE status = 'fraud'
