0215fe320681aa98923836e76b72f29661f2685a
saivarshitha891 <varshithasrinivas891@gmail.com>
2025-05-02 23:17:04 -0500

    Update credit_risk_scoring.sql

--- banking-risk-fraud-analytics/sql_queries/credit_risk_scoring.sql
+++ banking-risk-fraud-analytics/sql_queries/credit_risk_scoring.sql
@@ -1,6 +1,5 @@
--- Simulated credit score logic
+-- Simulate credit risk score based on total spending
 SELECT customer_id,
-       COUNT(*) AS total_transactions,
        SUM(amount) AS total_spent,
        CASE
          WHEN SUM(amount) > 50000 THEN 'Low Risk'
@@ -8,4 +7,16 @@ SELECT customer_id,
          ELSE 'High Risk'
        END AS credit_risk_score
 FROM synthetic_transactions
+GROUP BY customer_id;
+
+-- Score credit risk based on the frequency of fraud
+SELECT customer_id,
+       COUNT(*) AS fraud_count,
+       CASE
+         WHEN COUNT(*) > 5 THEN 'High Risk'
+         WHEN COUNT(*) BETWEEN 2 AND 5 THEN 'Medium Risk'
+         ELSE 'Low Risk'
+       END AS fraud_risk_score
+FROM synthetic_transactions
+WHERE status = 'fraud'
 GROUP BY customer_id;
