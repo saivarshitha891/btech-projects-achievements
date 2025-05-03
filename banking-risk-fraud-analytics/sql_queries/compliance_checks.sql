dbf0b7d9766cd60aa2b349c160169b8525cccd20
saivarshitha891 <varshithasrinivas891@gmail.com>
2025-05-02 23:21:02 -0500

    Update compliance_checks.sql

--- banking-risk-fraud-analytics/sql_queries/compliance_checks.sql
+++ banking-risk-fraud-analytics/sql_queries/compliance_checks.sql
@@ -1,3 +1,11 @@
--- Flag transactions over $10,000 (suspicious threshold)
+-- Flag transactions over $10,000
 SELECT * FROM synthetic_transactions
 WHERE amount > 10000;
+
+-- Identify customers with frequent withdrawals (e.g., more than 10 withdrawals)
+SELECT customer_id, COUNT(*) AS withdrawal_count
+FROM synthetic_transactions
+WHERE transaction_type = 'withdrawal'
+GROUP BY customer_id
+HAVING COUNT(*) > 10;
+
