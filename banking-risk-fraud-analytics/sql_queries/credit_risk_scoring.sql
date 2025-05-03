e170103fdb3ff02f26f1676a1c961d586b62a671
saivarshitha891 <varshithasrinivas891@gmail.com>
2025-05-03 06:36:40 -0500

    Update credit_risk_scoring.sql

--- banking-risk-fraud-analytics/sql_queries/credit_risk_scoring.sql
+++ banking-risk-fraud-analytics/sql_queries/credit_risk_scoring.sql
@@ -20,3 +20,34 @@ SELECT customer_id,
 FROM synthetic_transactions
 WHERE status = 'fraud'
 GROUP BY customer_id;
+
+-- Credit Risk Summary
+SELECT credit_risk_score, COUNT(*) AS num_customers
+FROM (
+    SELECT customer_id,
+           SUM(amount) AS total_spent,
+           CASE
+             WHEN SUM(amount) > 50000 THEN 'Low Risk'
+             WHEN SUM(amount) BETWEEN 20000 AND 50000 THEN 'Medium Risk'
+             ELSE 'High Risk'
+           END AS credit_risk_score
+    FROM synthetic_transactions
+    GROUP BY customer_id
+) AS credit_summary
+GROUP BY credit_risk_score;
+
+-- Fraud risk summary
+SELECT fraud_risk_score, COUNT(*) AS num_customers
+FROM (
+    SELECT customer_id,
+           COUNT(*) AS fraud_count,
+           CASE
+             WHEN COUNT(*) > 5 THEN 'High Risk'
+             WHEN COUNT(*) BETWEEN 2 AND 5 THEN 'Medium Risk'
+             ELSE 'Low Risk'
+           END AS fraud_risk_score
+    FROM synthetic_transactions
+    WHERE status = 'fraud'
+    GROUP BY customer_id
+) AS fraud_summary
+GROUP BY fraud_risk_score;
